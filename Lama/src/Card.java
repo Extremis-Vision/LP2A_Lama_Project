53b871a8a76a10be05acdf1ebef9face18f00e5f
Dridox <bastien.heitzmann@gmail.com>
2025-05-08 10:55:10 +0200

    Ajout des getter et setter

--- Lama/src/Card.java
+++ Lama/src/Card.java
@@ -3,16 +3,49 @@ package Card;
 public class Card {
     private int value;
     private String name;
+    private String PathtoImage;
 
-    public Card(int value, String name){
+    public Card(int value, String name) {
         this.value = value;
         this.name = name;
     }
 
+    // Getter pour value
+    public int getValue() {
+        return value;
+    }
+
+    // Setter pour value
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    // Getter pour name
+    public String getName() {
+        return name;
+    }
+
+    // Setter pour name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter pour PathtoImage
+    public String getPathtoImage() {
+        return PathtoImage;
+    }
+
+    // Setter pour PathtoImage
+    public void setPathtoImage(String PathtoImage) {
+        this.PathtoImage = PathtoImage;
+    }
+
+    @Override
     public String toString() {
         return "Card{" +
                 "value=" + value +
                 ", name='" + name + '\'' +
+                ", PathtoImage='" + PathtoImage + '\'' +
                 '}';
     }
 }
